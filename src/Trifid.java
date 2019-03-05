6473722588f8d04d7e9e49fa461a36436e7f4008
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-05 14:08:23 +0100

    Add method to decrypt characters to numbers

--- src/Trifid.java
+++ src/Trifid.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Trifid {
 
     public static void decrypt(String sentence) {
-        System.out.println(createAlgorithmBy3Squares());
-        System.out.println(getNumbersFromSentence(sentence));
+//        System.out.println(createAlgorithmBy3Squares());
+//        System.out.println(getNumbersFromSentence(sentence));
+        System.out.println(getDeryptedNumbers(5, sentence));
     }
 
     private static HashMap<String, Integer> createAlgorithmBy3Squares() {
@@ -28,19 +31,56 @@ public class Trifid {
         return squaresCipher;
     }
 
-    private static ArrayList<Integer> getNumbersFromSentence(String sentence) {
+    private static ArrayList<String> getNumbersFromSentence(String sentence) {
         HashMap<String, Integer> squaresCipher = createAlgorithmBy3Squares();
-        ArrayList<Integer> getDecryptedNumbers = new ArrayList<>();
+        ArrayList<String> getNumbersFromSentence = new ArrayList<>();
         System.out.println(sentence);
         char[] sentenceCharacters = sentence.replaceAll("\\s", "").toCharArray();
         System.out.println(sentenceCharacters);
 
         for (char character : sentenceCharacters) {
-            getDecryptedNumbers.add(squaresCipher.get("" + character));
+            getNumbersFromSentence.add(squaresCipher.get("" + character) + "");
         }
 
+        return getNumbersFromSentence;
+    }
+
+    private static ArrayList<String> getDeryptedNumbers(int period, String sentence) {
+        ArrayList<String> getNumbersFromSentence = getNumbersFromSentence(sentence);
+        ArrayList<String> getDecryptedNumbers = new ArrayList<>();
+
+        for (int from = 0; from <= getNumbersFromSentence.size(); from+=5) {
+            List<String> subListGetDecryptedNumbers = getNumbersFromSentence.subList(from, period);
+            period += 5;
+            if (period > getNumbersFromSentence.size()) {
+                period = getNumbersFromSentence.size();
+            }
+//            System.out.println(subListGetDecryptedNumbers.toString());
+            int iterator = 0;
+            String result = "";
+            for (int i = 0; i < 3; i++) {
+
+                int ii = 0;
+                while (ii < subListGetDecryptedNumbers.size()) {
+                    char[] charNumber = subListGetDecryptedNumbers.get(ii).toCharArray();
+                    if (result.length() != 3) {
+                        result += charNumber[iterator];
+                        ii++;
+                    }
+                    if (result.length() == 3){
+                        getDecryptedNumbers.add(result);
+                        result = "";
+                    }
+                }
+                iterator += 1;
+//                if (result.length() == 3) {
+//                    getDecryptedNumbers.add(result);
+//                }
+            }
+        }
         return getDecryptedNumbers;
     }
+
 
 
 
